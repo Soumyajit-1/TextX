bc84d4f2f7a556a914aaa1fc3c5649f30fa4b23e
Soumyajit Pal <soumyajitpal810@gmail.com>
2023-12-04 01:03:54 +0530

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '16.0'
+
+target 'TextX' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TextX
+
+pod 'CLTypingLabel'
+pod 'FirebaseAuth'
+pod 'FirebaseFirestore' 
+
+post_install do |installer|
+  installer.pods_project.targets.each do |target|
+    target.build_configurations.each do |config|
+      config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '16.0'
+    end
+  end
+end
+
+
+end
---
+++ TextX/Controllers/LoginViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LoginViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+
+    @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text , let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+              guard let strongSelf = self else { return }
+                if let e = error {
+                    print(e.localizedDescription)
+                }else{
+                    let vc = self?.storyboard?.instantiateViewController(withIdentifier: "ChatViewController") as! ChatViewController
+                    self?.navigationController?.pushViewController(vc, animated: true)
+                }
+              
+                
+            }
+        }
+    }
+    
+}
